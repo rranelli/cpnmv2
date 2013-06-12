676ed929139110433bd3978bf6a2b6526dcfbefb
Renan Ranelli <renanranelli@gmail.com>
2013-06-12 00:27:42 -0300

    implementando o mapeamento de item e projeto, e adicionando a Xref como forma de restringir os valores cadastrados

---
+++ CPNMv2/Domain/Xref.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPNMv2.Domain
+{
+    public class Xref : Entity
+    {
+        public virtual Property Property { get; set; }
+        public virtual ItemType ItemType { get; set; }
+    }
+}
