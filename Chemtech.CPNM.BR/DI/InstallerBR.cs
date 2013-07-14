1eb3431f7fb78e5e30c5cb923ad2f8dc9304f148
Renan Ranelli <renanranelli@gmail.com>
2013-07-14 16:38:08 -0300

    added everything that is needed to clone a project.

---
+++ Chemtech.CPNM.BR/DI/InstallerBR.cs
@@ -0,0 +1,13 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Chemtech.CPNM.BR.DI
+{
+    public class InstallerBR : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+        }
+    }
+}
